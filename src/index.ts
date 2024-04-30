87aeb938b502aa02a2131be1c4c889e6f4d1c806
jessica-sobreira <j.jecristina@gmail.com>
2024-04-30 20:19:26 -0300

    configurando index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
+import cors from "cors";
 import * as dotenv from "dotenv";
+import express from "express";
+import { usuarioRoutes } from "./routes/usuario.routes";
 dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/", usuarioRoutes());
+
+app.listen(process.env.PORT, () => {
+    console.log("API está rodando!");
+});
 
 
 
