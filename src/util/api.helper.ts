56e05728e6cb776517eecbe5067636bd1d39525e
Jéssica Sobreira <117686537+jessica-sobreira@users.noreply.github.com>
2024-05-03 16:13:40 -0300

    Update api.helper.ts

--- src/util/api.helper.ts
+++ src/util/api.helper.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import { usuarioRoutes } from "../routes/usuario.routes";
 import { loginRoutes } from "../routes/login.routes";
+import { tarefasRoutes } from "../routes/tarefa.routes";
 
 export function createApp() {
     const app = express();
@@ -10,8 +11,7 @@ export function createApp() {
 
     app.use("/usuario", usuarioRoutes());
     app.use("/login", loginRoutes());
-
-    // app.use("/tarefas", tarefasRoutes());
+    app.use("/tarefas", tarefasRoutes());
 
     return app;
 }
